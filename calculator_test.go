b22f382cc8451448d3f974e9bd4f078503593fcb
Max Lutch <lutchmax@gmail.com>
2025-02-11 20:02:41 +0200

    change the numbers

--- calculator_test.go
+++ calculator_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
+	var want float64 = 3
+	got := calculator.Subtract(5, 2)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
