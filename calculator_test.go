46a5cb1f20d6975050ece78d4b68d468572ea4c7
Max Lutch <lutchmax@gmail.com>
2025-02-13 15:07:56 +0200

    looped tests with slices

--- calculator_test.go
+++ calculator_test.go
@@ -27,18 +27,40 @@ func TestAdd(t *testing.T) {
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: -10, b: 2, want: -12},
+		{a: -1, b: -1, want: 0},
+		{a: 5, b: -5, want: 10},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f",
+				tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
-	var want float64 = 9
-	got := calculator.Multiply(3, 3)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: -10, b: 2, want: -20},
+		{a: -1, b: -1, want: 1},
+		{a: 5, b: -5, want: -25},
+	}
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f",
+				tc.a, tc.b, tc.want, got)
+		}
 	}
 }
